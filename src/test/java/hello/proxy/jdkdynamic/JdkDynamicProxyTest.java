69acfb21d2346df9caed7f5f617c68198954b4b1
kenokim <keno1413@gmail.com>
2023-01-29 22:27:36 +0900

    proxy

---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,17 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.AImpl;
+import hello.proxy.jdkdynamic.code.AInterface;
+import hello.proxy.jdkdynamic.code.TimeInvocationHandler;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+public class JdkDynamicProxyTest {
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        Object proxy = Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[] { AInterface.class }, handler);
+    }
+}
--- src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -2,12 +2,37 @@ package hello.proxy.jdkdynamic;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
     @Test
     void reflection0() {
         Hello target = new Hello();
-        target.callA();
-        target.callB();
+        // 공통 로직 1 시작
+        System.out.println("start");
+        String result1 = target.callA();
+        System.out.println(result1);
+        // 공통 로직 1 종료
+
+        // 공통 로직 2 시작
+        System.out.println("start");
+        String result2 = target.callB();
+        System.out.println(result2);
+        // 공통 로직 2 종료
+    }
+
+    @Test
+    void reflection1() throws Exception {
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+        Hello target = new Hello();
+        Method callA = classHello.getMethod("callA");
+        Method callB = classHello.getMethod("callB");
+
+        Object result1 = callA.invoke(target);
+        System.out.println("result 1 = " + result1);
+
+        Object result2 = callB.invoke(target);
+        System.out.println("result 2 = " + result2);
     }
 
     static class Hello {
